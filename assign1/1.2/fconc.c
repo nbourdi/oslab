d5b1fc95cf22336b31829fa8f5312722080bf163
nat <62358292+nbourdi@users.noreply.github.com>
2022-03-18 22:21:28 +0200

    Create fconc.c

---
+++ assign1/1.2/fconc.c
@@ -0,0 +1,20 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+// writes in fd file 
+void doWrite(int fd, const char *buff, int len) { 
+
+}
+
+// writes the contents of the file named infile in the descriptor of fd, uses doWrite()
+void write_file(int fd, const char *infile) {
+
+}
+
+// in this case argv[1] is infile1, argv[2] infile2, argv[3] outfile
+int main (int argc, char **argv) {
+ }
